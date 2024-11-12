425ed023e4820e9e821c996b313b2d0035420c4d
prakashbmdigital <prakeshkumar0580@gmail.com>
2024-11-12 16:51:27 +0530

    updated

--- lib/home_page/entrance_preparation/components/review_card.dart
+++ lib/home_page/entrance_preparation/components/review_card.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
