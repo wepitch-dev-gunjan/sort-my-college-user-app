d9fcac0e571d88b3aac70fd68aed56e66f841e0b
prakashbmdigital <prakeshkumar0580@gmail.com>
2025-04-28 17:56:31 +0530

    update files and code

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -12,45 +12,35 @@ void navigationPop() {
 }
 
 
-removeUntilRoute(BuildContext context, String pageName, {Object? arguments}) {
-  Navigator.pushNamedAndRemoveUntil(
-      context, pageName, arguments: arguments, (route) => false);
-}
+// removeUntilRoute(BuildContext context, String pageName, {Object? arguments}) {
+//   Navigator.pushNamedAndRemoveUntil(
+//       context, pageName, arguments: arguments, (route) => false);
+// }
 
-pushReplaceRoute(BuildContext context, String pageName, {Object? arguments}) {
-  Navigator.pushReplacementNamed(context, pageName, arguments: arguments);
-}
+// pushReplaceRoute(BuildContext context, String pageName, {Object? arguments}) {
+//   Navigator.pushReplacementNamed(context, pageName, arguments: arguments);
+// }
 
-pushRoute(BuildContext context, String pageName, {Object? arguments}) {
-  Navigator.pushNamed(context, pageName, arguments: arguments);
-}
+// pushRoute(BuildContext context, String pageName, {Object? arguments}) {
+//   Navigator.pushNamed(context, pageName, arguments: arguments);
+// }
 
-//  Naviation
-// navigationRemoveUntil( Widget widget) {
+
+// Future<void> navigationRemoveUntil(Widget widget) {
 //   return navigatorKey.currentState!.pushAndRemoveUntil(
-//       MaterialPageRoute(builder: (context) => widget),
-//       (Route<dynamic> route) => false);
-// }
-
-Future<void> navigationRemoveUntil(Widget widget) {
-  return navigatorKey.currentState!.pushAndRemoveUntil(
-    MaterialPageRoute(builder: (context) => widget),
-    (route) => false,
-  );
-}
-
-// navigationPushReplacement(BuildContext context, Widget widget) {
-//   Navigator.pushReplacement(
-//       context, MaterialPageRoute(builder: (context) => widget));
+//     MaterialPageRoute(builder: (context) => widget),
+//     (route) => false,
+//   );
 // }
 
 
-Future<dynamic> navigatePushReplacement(Widget widget) {
-  return navigatorKey.currentState!
-      .pushReplacement(MaterialPageRoute(builder: (context) => widget));
-}
 
-Future navigationPush(BuildContext context, Widget widget) {
-  return Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => widget));
-}
+// Future<dynamic> navigatePushReplacement(Widget widget) {
+//   return navigatorKey.currentState!
+//       .pushReplacement(MaterialPageRoute(builder: (context) => widget));
+// }
+
+// Future navigationPush(BuildContext context, Widget widget) {
+//   return Navigator.of(context)
+//       .push(MaterialPageRoute(builder: (context) => widget));
+// }
